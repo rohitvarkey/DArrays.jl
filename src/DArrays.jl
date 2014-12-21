079cd83dabab1ddb1e0176633f388e1b34f29513
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2014-12-22 01:42:59 +0530

    Implemented the .- operator

--- src/DArrays.jl
+++ src/DArrays.jl
@@ -3,4 +3,8 @@ module DArrays #operations on DArrays
 function .+{T}(A::DArray{T},B::Number)
         map(r->r+B,A)
     end
+
+function .-{T}(A::DArray{T},B::Number)
+        map(r->r-B,A)
+    end
 end
