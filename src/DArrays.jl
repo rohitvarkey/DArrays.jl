49f54ee5b35fd6f8bae4023e05978fc1ae26d9f0
Sushant <sushant.dinesh94@gmail.com>
2014-12-22 14:01:13 +0530

    Added mappart to make operators faster

--- src/DArrays.jl
+++ src/DArrays.jl
@@ -1,12 +1,12 @@
 module DArrays
 
-import Base
+import Base.Callable
+
+mappart(f::Callable, d::DArray) = DArray(I->f(localpart(d)), d)
 
 for f in (:.+, :.-, :.*, :./, :.%, :.<<, :.>>, :div, :mod, :rem, :&, :|, :$)
     @eval begin
-        function ($f){T}(A::DArray{T}, B::Number)
-            map(r->($f)(r, B), A)
-        end
+        ($f){T}(A::DArray{T}, B::Number) = mappart(r->($f)(r, B), A)
     end
 end
 
