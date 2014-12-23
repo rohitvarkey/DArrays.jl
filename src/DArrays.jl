a177b022ca35bda1cbbd88e3f8a61877f6394c40
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2014-12-23 16:55:25 +0530

    Added the + function.

--- src/DArrays.jl
+++ src/DArrays.jl
@@ -10,4 +10,13 @@ for f in (:.+, :.-, :.*, :./, :.%, :.<<, :.>>, :div, :mod, :rem, :&, :|, :$)
     end
 end
 
+function + {S,T}(A::DArray{S},B::DArray{T})
+    assert(A.dims == B.dims)
+    if A.pmap == B.pmap && A.indexes == B.indexes
+        return mappart(r->r+localpart(B),A)
+    else
+        reshape(distribute(map(r->r[1]+r[2],zip(A,B))),A.dims)
+    end
+end
+
 end
